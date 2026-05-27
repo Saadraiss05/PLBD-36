e17171acd9d34273ee7eaa2806dbad3beeec47f4
Saadraiss05 <raissaad26@gmail.com>
2026-05-27 18:31:16 +0100

    Add modules/__init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,11 +1,2 @@
 # modules/__init__.py
-from .vision import VisionPlante
-from .capteur_humidite import CapteurHumidite
-from .capteur_ultrasons import CapteurUltrasons
-from .capteur_niveau_eau import CapteurNiveauEau
-from .pompe_eau import PompeEau
-from .moteurs import Moteurs
-from .suiveur_ligne import SuiveurLigne
-
-__all__ = ['VisionPlante','CapteurHumidite','CapteurUltrasons',
-           'CapteurNiveauEau','PompeEau','Moteurs','SuiveurLigne']
+# Package modules du robot PLBD-36
